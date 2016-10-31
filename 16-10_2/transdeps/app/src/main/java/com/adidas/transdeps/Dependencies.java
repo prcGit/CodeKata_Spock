cfc2293d9abf5d206a4be6989e23ca632b88d161
Valery Miskevich <valery.miskevich@gmail.com>
2016-10-31 13:01:57 +0300

    1st working version

--- 16-10_2/transdeps/app/src/main/java/com/adidas/transdeps/Dependencies.java
+++ 16-10_2/transdeps/app/src/main/java/com/adidas/transdeps/Dependencies.java
@@ -1,12 +1,9 @@
 package com.adidas.transdeps;
 
-import android.util.ArraySet;
-
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -15,27 +12,41 @@ public class Dependencies {
 
     public static String[] findFullDependencies(String[] direct) {
 
+        // read input to map
         Map<String, Set<String>> map = new HashMap<>();
-
         for (String rule : direct) {
             String[] items = rule.split(" ");
-
-            Set<String> deps = new HashSet<String>(items.length - 1);
+            Set<String> deps = new HashSet<>(items.length - 1);
             for (int i = 1; i < items.length; i++) {
                 deps.add(items[i]);
             }
             map.put(items[0], deps);
         }
 
-        for (Iterator<String> it = map.keySet().iterator(); it.hasNext(); ) {
-            addDeps(it.next(), map);
+        // find indirect deps and put to a new map
+        Map<String, Set<String>> indirectDepsMap = new HashMap<>();
+        for (String item : map.keySet()) {
+            Set<String> indirectDeps = new HashSet<>();
+            addDeps(item, indirectDeps, map);
+            indirectDepsMap.put(item, indirectDeps);
+        }
+        // merge direct and indirect deps
+        for (String item : map.keySet()) {
+            Set<String> deps = map.get(item);
+            Set<String> indirectDeps = indirectDepsMap.get(item);
+            if (indirectDeps != null) {
+                deps.addAll(indirectDeps);
+            }
         }
 
+        // output
         String[] result = new String[map.size()];
         int index = 0;
         for (String item : map.keySet()) {
             StringBuilder sb = new StringBuilder(item);
-            for (String dep : map.get(item)) {
+            List<String> deps = new ArrayList<>(map.get(item));
+            Collections.sort(deps);
+            for (String dep : deps) {
                 sb.append(" ").append(dep);
             }
             result[index] = sb.toString();
@@ -45,11 +56,14 @@ public class Dependencies {
         return result;
     }
 
-    private static void addDeps(String item, Map<String, Set<String>> items) {
+    private static void addDeps(String item, Set<String> addTo, Map<String, Set<String>> items) {
         Set<String> deps = items.get(item);
         for (String dep : deps) {
             Set<String> depDeps = items.get(dep);
-            deps.addAll(depDeps);
+            if (depDeps != null) {
+                addTo.addAll(depDeps);
+                addDeps(dep, addTo, items);
+            }
         }
     }
 }
